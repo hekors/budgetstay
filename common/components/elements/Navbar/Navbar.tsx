c00c17a6d60c6586f6c5b8cc5f473c805bb175ec
Yash Sehgal <yashsehgal.work@gmail.com>
2023-02-04 01:10:41 +0530

    feat: chakra-ui, navbar added

---
+++ common/components/elements/Navbar/Navbar.tsx
@@ -0,0 +1,11 @@
+import { Container } from "@chakra-ui/react";
+
+const Navbar: React.FunctionComponent = () => {
+    return (
+        <Container>
+            
+        </Container>
+    )
+};
+
+export default Navbar;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,16 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { ChakraProvider } from '@chakra-ui/react';
+
+import Navbar from '../common/components/elements/Navbar/Navbar'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ChakraProvider>
+      <Navbar />
+      <Component {...pageProps} />
+    </ChakraProvider>
+  )
 }
 
 export default MyApp
