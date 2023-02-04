39bdc249e791f37c53e0493453be2a5a8b34de76
Ayush Soni <ayushsoni1010.work@gmail.com>
2023-02-04 21:53:39 +0530

    feat: added custom 500 page

---
+++ pages/500.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+import NextLink from "next/link";
+import {
+  Box,
+  Button,
+  Container,
+  Divider,
+  Heading,
+  Text,
+} from "@chakra-ui/react";
+
+import BaseLayout from "../common/components/layouts/BaseLayout";
+
+const Custom500 = () => {
+  return (
+    <React.Fragment>
+      <BaseLayout>
+        <Container m="auto" mt="20">
+          <Heading as="h1">Internal server error</Heading>
+          <Text mt="2">Oops, looks like we broke something...</Text>
+          <Text>An unexpected error occured</Text>
+          <Heading fontSize="4xl" my="4">
+            ❌ 500
+          </Heading>
+          <Divider my="6" />
+          <Box my="6">
+            <NextLink href="/" passHref>
+              <Button
+                type="button"
+                variant="solid"
+                colorScheme="blue"
+                bgGradient="linear(to-r, blue.400, blue.700)"
+              >
+                This way back to safety, please ❤️
+              </Button>
+            </NextLink>
+          </Box>
+        </Container>
+      </BaseLayout>
+    </React.Fragment>
+  );
+};
+
+export default Custom500;
