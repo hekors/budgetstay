73a510f6a1077448a0e4e3ff8c4735223e565f23
Yash Sehgal <yashsehgal.work@gmail.com>
2023-02-04 13:39:07 +0530

    feat: owner view added

---
+++ pages/owner/new/index.jsx
@@ -0,0 +1,12 @@
+import { Box, Heading, Text } from "@chakra-ui/react";
+import BaseLayout from "../../../common/components/layouts/BaseLayout";
+
+const NewProperty = () => {
+    return (
+        <BaseLayout>
+            <Heading fontSize={"xl"}>{"Listing New Property"}</Heading>
+        </BaseLayout>
+    )
+};
+
+export default NewProperty;
