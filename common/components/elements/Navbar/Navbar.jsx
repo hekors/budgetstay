0df0b099a3acd39a0aaa00f04ff7006abc232e41
Yash Sehgal <yashsehgal.work@gmail.com>
2023-02-04 14:17:58 +0530

    feat: new property listing form created

--- common/components/elements/Navbar/Navbar.jsx
+++ common/components/elements/Navbar/Navbar.jsx
@@ -1,21 +1,28 @@
 import { Box, Button, ButtonGroup, Text } from "@chakra-ui/react";
 import Image from "next/image";
+import Link from "next/link";
 import BaseLayout from '../../layouts/BaseLayout';
 
 const Navbar = () => {
     return (
         <Box bgColor={"black"}>
             <BaseLayout flexDirection={"row"} display={"flex"} alignItems={"center"} justifyContent={"space-between"}>
-                <Image 
-                    src={"/icons/logo.svg"}
-                    width={"140"}
-                    height={"80"}
-                    alt="logo"
-                    priority
-                />
+                <Link href="/">
+                    <Image 
+                        src={"/icons/logo.svg"}
+                        width={"140"}
+                        height={"80"}
+                        alt="logo"
+                        priority
+                    />
+                </Link>
                     <ButtonGroup>
-                        <Button colorScheme={"blue"} variant={"ghost"}>Login</Button>
-                        <Button variant={"solid"} colorScheme={"blue"}>Create Account</Button>
+                        <Link href={"/login"}>
+                            <Button colorScheme={"blue"} variant={"ghost"}>Login</Button>
+                        </Link>
+                        <Link href={"/signup"}>
+                            <Button variant={"solid"} colorScheme={"blue"}>Create Account</Button>
+                        </Link>
                     </ButtonGroup>
             </BaseLayout>
         </Box>
